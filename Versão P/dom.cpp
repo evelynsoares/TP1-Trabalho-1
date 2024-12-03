5df4b4bc1e67b88420a52b1df7deb515fe7ad2c4
Pedro <222011580@aluno.unb.br>
2024-12-03 10:46:58 -0300

    Mais validar

--- Versão P/dom.cpp
+++ Versão P/dom.cpp
@@ -28,9 +28,22 @@ bool Codigo::validar(string codigo){
 }
 
 bool Data::validar(u_int8_t DD, u_int8_t MM, u_int8_t AA){
-    if(DD < 1 || DD > 31 || MM > 12 || MM < 1 || (DD > 30 && ((MM%2 == 0 && MM < 8) || (MM%2 && MM > 7)))){
+    if(DD < 1 || DD > 31 || MM > 12 || MM < 1 || (DD > 30 && ((MM%2 == 0 && MM < 8) || (MM%2 && MM > 7))) || (AA%4 == 0 && MM == 2 && DD > 29)){
         return false;
     }
     return true;
 }
 
+bool Dinheiro::validar(double m){
+    if(m > 200000 || m < 0){
+        return false;
+    }
+    return true;
+}
+
+bool Duracao::validar(u_int16_t d){
+    if(d > 360 || d < 0){
+        return false;
+    }
+    return true;
+}
