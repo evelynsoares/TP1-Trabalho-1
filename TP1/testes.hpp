8a1ba0a34937f46709eb6a12ebefca512349615c
victorgiraoc <37477068+victorgiraoc@users.noreply.github.com>
2024-12-08 20:54:10 -0300

    adicionado testes unitários de dominios

---
+++ TP1/testes.hpp
@@ -0,0 +1,151 @@
+#ifndef TESTES_HPP_INCLUDED
+#define TESTES_HPP_INCLUDED
+
+#include <stdexcept>
+
+#include "dominios.hpp"
+
+using namespace std;
+
+class TUCodigo {
+    private:
+        const static string VALOR_VALIDO =  "Az«301" ;
+        const static string VALOR_INVALIDO = "1234567";
+        const static string VALOR_INVALIDO2 = "ab";
+        const static string VALOR_INVALIDO3 = "!567)";
+        Codigo *codigo;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioValorValido();
+        void testerCenarioValorInvalido();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+};
+
+
+class TUAvaliacao {
+    private:
+        const static int VALOR_VALIDO =  5 ;
+        const static int VALOR_INVALIDO = -1;
+        const static int VALOR_INVALIDO2 = 6;
+        const static int VALOR_INVALIDO3 = 1000;
+        Avaliacao *avaliacao;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioValorValido();
+        void testerCenarioValorInvalido();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+};
+
+class TUData {
+    private:
+        const static int VALOR_VALIDO[3] =  {29, 2, 2024} ;
+        const static int VALOR_INVALIDO[3] = {31, 4, 2020};
+        const static int VALOR_INVALIDO2[3] = {29, 2, 1900};
+        const static int VALOR_INVALIDO3[3] = {29, 2, 2023};
+        Data *data;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioValorValido();
+        void testerCenarioValorInvalido();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+};
+
+
+class TUDinheiro {
+    private:
+        const static double VALOR_VALIDO = 1000.55;
+        const static double VALOR_INVALIDO = 200001;
+        const static double VALOR_INVALIDO2 = -55.;
+        Dinheiro *dinheiro;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioValorValido();
+        void testerCenarioValorInvalido();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+};
+
+class TUDuracao {
+    private:
+        const static int VALOR_VALIDO = 220;
+        const static int VALOR_INVALIDO = 361;
+        Duracao *duracao;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioValorValido();
+        void testerCenarioValorInvalido();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+};
+
+
+class TUHorario {
+    private:
+        const static int VALOR_VALIDO[2] = {22, 32};
+        const static int VALOR_INVALIDO[2] = {24, 00};
+        Horario *horario;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioValorValido();
+        void testerCenarioValorInvalido();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+};
+
+class TUNome {
+    private:
+        const static char VALOR_VALIDO[30] = "nao sei o que escrever";
+        const static char VALOR_INVALIDO[30] = "1234567890123456789012345678901";
+        Nome *nome;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioValorValido();
+        void testerCenarioValorInvalido();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+};
+
+class TUSenha {
+    private:
+        const static char VALOR_VALIDO =  86259;
+        const static char VALOR_INVALIDO = 12345;
+        const static char VALOR_INVALIDO2 = 54321;
+        const static char VALOR_INVALIDO3 = 862590;
+        const static char VALOR_INVALIDO4 = 86258;
+        Senha *senha;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenarioValorValido();
+        void testerCenarioValorInvalido();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
+};
+
+#endif // TESTES_HPP_INCLUDED
