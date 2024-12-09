b1e5c88b71f254079532dbbc9684c57b9a9b0a85
OutsiderRat <88382062+OutsiderRat@users.noreply.github.com>
2024-12-08 22:15:05 -0300

    Update dominios.hpp
    só meti comentários

--- TP1/dominios.hpp
+++ TP1/dominios.hpp
@@ -5,57 +5,111 @@
 
 using namespace std;
 
+///
+/// Avaliação possui uma nota de 0 a 5
+///
 class Avaliacao {
 public:
+///
+/// @param nota digito 0,1,2,3,4 ou 5
+///
     int nota;
     bool validar(int);
     Avaliacao(int);
 };
 
+///
+/// A classe código se refere a chave primária de uma viagem
+///
 class Codigo {
 public:
+///
+/// @param codigo string de 6 caracteres (letras ou digitos)
+///
     string codigo;
+///
+/// @param validar
+///
     bool validar(string);
     Codigo(string);
 };
 
+///
+/// A classe Data refere-se a data temporal (dia/mês/ano)
+///
 class Data {
 public:
+///
+/// @param DD Dia (1 - 31, conforme mês adequado)
+/// @param MM Mês ( 1 - 12)
+/// @param AA Ano (apenas os dois últimos digitos de um ano, 00 - 99)
+///
     int DD, MM, AA;
     bool validar(int, int, int);
     Data(int d, int m, int a);
 };
 
+///
+/// Dinheiro refere-se ao valor de uma atividade ou hospedagem no sistema
+///
 class Dinheiro {
 public:
+///
+/// @param valor ( 00,00 - 200.000,00)
+///
     double valor;
     bool validar(double);
     Dinheiro(double);
 };
 
+///
+/// Duração de alguma atividade em dias
+///
 class Duracao {
 public:
+///
+/// @param tempo quantidade de dias de uma atividade (0 - 360))
+///
     int tempo;
     bool validar(int);
     Duracao(int);
 };
 
+///
+/// A Classe Horário denota o horário de alguma atividade do sistema
+///
 class Horario {
 public:
+///
+/// @param HH Horas (00 - 23)
+/// @param MM Minutos (00 - 59)
+///
     int HH, MM;
     bool validar(int, int);
     Horario(int h, int m);
 };
 
+///
+/// Nome de uma viagem, destino, ativididade ou hospedagem.
+///
 class Nome {
 public:
+///
+/// @param nome texto com até 30 caracteres.
+///
     char nome[30];
     bool validar(char *);
     Nome(char *);
 };
 
+///
+/// Senha para o acesso de uma conta.
+///
 class Senha {
 public:
+///
+/// @param senha Cinco digitos, sem digito duplicado ou em ordem crescente/decrescente.
+///
     unsigned int senha;
     bool validar(unsigned int);
     Senha(unsigned int);
